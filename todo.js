7109632e758258509f8a6c8a4d6c06f92464d885
Moong0122 <2015004802@hanyang.ac.kr>
2019-11-25 09:34:33 +0900

    remove To Do List elements in local storage

--- todo.js
+++ todo.js
@@ -3,7 +3,22 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
+
+function filterFn(toDo){
+    return toDo.id === 1;
+}
+
+function deleteToDo(event){
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();
+}
 
 function saveToDos(){
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
@@ -15,6 +30,7 @@ function paintToDO(text){
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "X";
+    delBtn.addEventListener("click",deleteToDo);
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
