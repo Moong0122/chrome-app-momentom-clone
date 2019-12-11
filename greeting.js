0e191e0b39b5c4ffe132dfc02d18631ab4395664
Moong0122 <2015004802@hanyang.ac.kr>
2019-12-11 23:54:14 +0900

    divide the name and greeting

--- greeting.js
+++ greeting.js
@@ -1,9 +1,8 @@
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings"),
-  userName = document.getElementById("changeName");
-
-console.log(userName);
+  userName = document.getElementById("changeName"),
+  nameBlank = "\u00A0";
 
 var greetMessage = ["Hello", "Have a nice day", "You're doing great"],
   MS_NUMBER = 3;
@@ -16,6 +15,10 @@ function randomNum() {
   const number = Math.floor(Math.random() * MS_NUMBER);
   // console.log(number);
   return number;
+}
+
+function userLogout() {
+  localStorage.removeItem(USER_LS);
 }
 
 function saveName(text) {
@@ -41,11 +44,12 @@ function paintGreeting(text) {
   userName.classList.add(SHOWING_CN);
   // 이름만 hover효과주기 적용중
   userName.classList.add(SHOWING_HOVER);
-  greeting.innerText = greetMessage[randomNum()] + ", ";
+  greeting.textContent = `${greetMessage[randomNum()]},${nameBlank}`;
   userName.innerText = text;
 }
 
 function loadName() {
+  userName.addEventListener("click", userLogout);
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
     askForName();
